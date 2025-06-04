cfd43cec47ba1c5eea583e3bfb4461108a84d6c6
Andrew Sheimo <andy@sheimo.net>
2025-06-04 11:30:44 -0400

    making led blink during es restart

--- SafeShutdown.py
+++ SafeShutdown.py
@@ -39,6 +39,7 @@ def when_released():
  led.on()
 
 def reboot():
+ led.blink()
  output = int(subprocess.check_output(['/opt/RetroFlag/multi_switch.sh', '--es-pid']))
  output_rc = int(subprocess.check_output(['/opt/RetroFlag/multi_switch.sh', '--rc-pid']))
  if output_rc:
@@ -47,7 +48,8 @@ def reboot():
      os.system("/opt/RetroFlag/multi_switch.sh --es-restart")
  else:
      os.system("sudo reboot")
- 
+ led.on()
+
 btn = Button(powerPin, hold_time=hold)
 rebootBtn = Button(resetPin)
 rebootBtn.when_pressed = reboot 
