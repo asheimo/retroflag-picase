dc9ab7ccff7e06d8f3995b61b98173e8ee43d84b
Andrew Sheimo <andy@sheimo.net>
2025-06-04 14:14:38 -0400

    updated shutdown to blink led until poweroff

--- SafeShutdown.py
+++ SafeShutdown.py
@@ -9,17 +9,17 @@ Device.pin_factory = PiGPIOFactory()
 
 from gpiozero import Button, LED
 from gpiozero.pins.pigpio import PiGPIOFactory
-import os 
+import os
 from signal import pause
 import subprocess
 
 # Use pigpio factory
 factory = PiGPIOFactory()
 
-powerPin = 3 
-resetPin = 2 
-ledPin = 14 
-powerenPin = 4 
+powerPin = 3
+resetPin = 2
+ledPin = 14
+powerenPin = 4
 hold = 1
 led = LED(ledPin)
 led.on()
@@ -28,13 +28,14 @@ power.on()
 
 #functions that handle button events
 def when_pressed():
- led.blink(.2,.2)
- output = int(subprocess.check_output(['/opt/RetroFlag/multi_switch.sh', '--es-pid']))
- if output:
-     os.system("/opt/RetroFlag/multi_switch.sh --es-poweroff")
- else:
-     os.system("sudo shutdown -h now")
-    
+    led.blink(on_time=0.2, off_time=0.2)  # Blink continuously
+
+    output = int(subprocess.check_output(['/opt/RetroFlag/multi_switch.sh', '--es-pid']))
+    if output:
+        os.system("/opt/RetroFlag/multi_switch.sh --es-poweroff")
+    else:
+        os.system("sudo shutdown -h now")
+
 def when_released():
  led.on()
 
@@ -66,10 +67,9 @@ def reboot():
     else:
         os.system("sudo reboot")
 
-
 btn = Button(powerPin, hold_time=hold)
 rebootBtn = Button(resetPin)
-rebootBtn.when_pressed = reboot 
+rebootBtn.when_pressed = reboot
 btn.when_pressed = when_pressed
 btn.when_released = when_released
 pause()
